b72ebaeca2960b743411aae8812e2e9046b10965
suhaibqaiser <suhaibqaiser@yahoo.com>
2016-12-13 20:22:12 -0400

    quiz page

--- public/js/controllers/QuizCtrl.js
+++ public/js/controllers/QuizCtrl.js
@@ -1,5 +1,33 @@
 app.controller('QuizController', function($scope) {
 
-	$scope.tagline = 'The square root of life is pi!';	
+	$scope.tagline = 'The square root of life is pi!';
+	$scope.isError = false;
+    $scope.data = {
+        "questions" : [
+            {
+                "name" : "what is the color in sky",
+                "answers" : [ "Blue", "Red", "Green", "Yellow"
+                ]
+            },
+            {
+                "name" : "which floor is shiftkeylabs",
+                "answers" : [ "1", "2", "3", "4"
+                ]
+            }
+        ]
+    }
+	$scope.quesNumber = 1;
+
+    $scope.questionText = $scope.data.questions[$scope.quesNumber - 1].name;
+    $scope.answers = $scope.data.questions[$scope.quesNumber - 1].answers;
+
+	$scope.nextQuestion = function () {
+		$scope.questionText = $scope.data.questions[$scope.quesNumber].name;
+		$scope.answers = $scope.data.questions[$scope.quesNumber].answers;
+        $scope.isError = true;
+        $scope.quesNumber = $scope.quesNumber + 1;
+    }
+
+
 
 });
